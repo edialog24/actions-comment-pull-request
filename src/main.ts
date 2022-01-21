2dc48f061d6a112b80b7429421c44febe1780358
Øyvind <iforwardmails@gmail.com>
2022-01-21 16:50:24 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,14 +13,12 @@ async function run() {
         return;
     }
     const pull_request_number = context.payload.pull_request.number;
-    //const pull_request_reviewers = context.payload.pull_request.requested_reviewers.users[0].login;
-    const pull_request_reviewers = 1;//context.payload.pull_request.requested_reviewers.users.length;
 
     const octokit = new github.GitHub(github_token);
     const new_comment = octokit.issues.createComment({
         ...context.repo,
         issue_number: pull_request_number,
-        body: "tepp"
+        body: message
       });
 
   } catch (error) {
